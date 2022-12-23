58a99bfd002f22788b5561722bdc594658410e3d
nahidarupa034034 <115896529+nahidarupa034034@users.noreply.github.com>
2022-12-24 02:55:38 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.banner_slider').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        speed:1000,
+        arrows:false
+        
+        
+    })
+})
+$(function () {
+    $('.beauty_product_slider').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        speed:1000,
+        arrows:false,
+        slidesToShow:4
+
+        
+        
+    })
+})
